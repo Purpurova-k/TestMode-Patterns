11d522bb822026e1ec63d3d6ebbb870906897f5d
Purpurova-k <katrin.purpurova@gmail.com>
2022-02-09 12:27:01 +0300

    Data class & generator added

---
+++ src/test/java/ru/netology/utils/DataGenerator.java
@@ -0,0 +1,77 @@
+package ru.netology.utils;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.experimental.UtilityClass;
+import ru.netology.entities.RegistrationInfo;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+@UtilityClass
+public class DataGenerator {
+
+    Faker faker = new Faker(new Locale("en"));
+
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+
+    @UtilityClass
+    public static class Registration {
+
+        public static RegistrationInfo activeUser() {
+            return new RegistrationInfo(
+                    faker.name().firstName(),
+                    faker.internet().password(),
+                    "active");
+        }
+
+        public static RegistrationInfo blockedUser() {
+            return new RegistrationInfo(
+                    faker.name().firstName(),
+                    faker.internet().password(),
+                    "blocked");
+        }
+
+
+        public static void registerActiveUser() {
+            given()
+                    .spec(requestSpec)
+                    .body(activeUser())
+                    .when()
+                    .post("/api/system/users")
+                    .then()
+                    .statusCode(200);
+        }
+
+
+        public static void registerBlockedUser() {
+            given()
+                    .spec(requestSpec)
+                    .body(blockedUser())
+                    .when()
+                    .post("/api/system/users")
+                    .then()
+                    .statusCode(200);
+        }
+    }
+
+
+    public static String generateWrongLogin() {
+       return faker.name().firstName();
+    }
+
+    public static String generateWrongPassword() {
+        return faker.internet().password();
+    }
+}
